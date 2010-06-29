dd45530503cd54c7b3129fd5253f6dd63d5a7b15
emanuel.papp <emanuel.papp@2e22713b-2209-b7e7-296a-dad1deb565c9>
2010-06-29 01:44:01 +0000

    Adding files that were not in version control.

---
+++ trunk/TripleThreatWF1.0/TripleThreatWF/Models/LenderModel.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using TripleThreat.Framework.Core;
+
+namespace TripleThreatWF.Models
+{
+    public class LenderModel
+    {
+        public string LenderName { get; set; }
+        public string AccountNumber { get; set; }
+        public string Street { get; set; }
+        public string City { get; set; }
+        public string State { get; set; }
+        public string ZipCode { get; set; }
+        public Lender Lender { get; set; }
+        public List<Lender> Lenders { get; set; }
+    }
+}
