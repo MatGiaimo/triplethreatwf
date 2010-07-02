7de32e44a6be1812a2e495db262befaf40e6d14c
mgiaimo <mgiaimo@2e22713b-2209-b7e7-296a-dad1deb565c9>
2010-07-02 03:15:06 +0000

    Added some simple validation to Add Document.

---
+++ TripleThreatWF1.0/TripleThreat.Framework/Core/Document.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace TripleThreat.Framework.Core
+{
+    public partial class Document
+    {
+        public bool Validate()
+        {
+            if (this.Folder == null || this.Customer == null)
+                return false;
+            return true;
+        }
+
+    }
+}
--- TripleThreatWF1.0/TripleThreatWF/Models/SearchModel.cs
+++ TripleThreatWF1.0/TripleThreatWF/Models/SearchModel.cs
@@ -4,12 +4,15 @@ using System.Linq;
 using System.Web;
 using System.Web.Mvc;
 using TripleThreat.Framework.Core;
+using System.ComponentModel.DataAnnotations;
 
 namespace TripleThreatWF.Models
 {
     public class SearchModel
     {
         public string Name { get; set; }
+        [Required]
+        [DataType(DataType.Date)]
         public DateTime? DateAdded { get; set; }
         public string CustName { get; set; }
         public string State { get; set; }
